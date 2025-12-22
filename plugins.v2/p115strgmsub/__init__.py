e41b7b6a49112b15e51c1e1072fbadc437c2ed2d
mrtian2016 <mrtian2016@gmail.com>
2025-12-23 02:07:18 +0800

    update

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -47,7 +47,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.0.9"
+    plugin_version = "1.0.8"
     # 插件作者
     plugin_author = "mrtian2016"
     # 作者主页
