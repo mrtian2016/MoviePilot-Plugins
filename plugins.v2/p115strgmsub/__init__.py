7bcdad394f11c444577493ad0833360bc230a2ce
frankk007 <43888225+frankk007@users.noreply.github.com>
2026-01-24 01:35:42 +0800

    Update __init__.py

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -42,7 +42,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.2.0"
+    plugin_version = "1.2.1"
     # 插件作者
     plugin_author = "frankk007"
     # 作者主页
